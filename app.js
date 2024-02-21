bfd046dd30b6f51024db8659600e37b8957704be
1jds <jamesdstacey2@gmail.com>
2024-02-21 11:05:06 +1100

    rolled back saveUninitialized to false

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ app.use(
     }),
     secret: process.env.EXPRESS_SESSION_SECRET_KEY,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
       maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days session timeout
     },
