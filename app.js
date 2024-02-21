e3ca9236fe0923039744369cb16d31b364fe780e
1jds <jamesdstacey2@gmail.com>
2024-02-21 11:47:39 +1100

    moving to minimal product for testing

--- app.js
+++ app.js
@@ -64,6 +64,10 @@ passport.use(
         }
         // Check if the user exists
         const user = result.rows[0];
+        console.log(
+          "The user details while authenticating local strategy are... :",
+          user
+        );
         if (!user) {
           return done(null, false);
         }
@@ -72,6 +76,9 @@ passport.use(
           return done(null, false);
         }
         // If the username and password are correct, return the user
+        console.log(
+          "In the local strategy middleware, and everything looks good..."
+        );
         return done(null, user);
       }
     );
@@ -125,6 +132,10 @@ app.use(passport.session());
 // -----------------  ROUTING  ----------------- //
 // --------------------------------------------- //
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the nightlife server!");
+});
+
 app.get("/current-session", passport.authenticate("session"), (req, res) => {
   // console.log(
   //   "Here is the req.session for /current-session......... :",
@@ -149,43 +160,43 @@ app.get("/current-session", passport.authenticate("session"), (req, res) => {
   }
 });
 
-app.post("/register", (req, res) => {
-  console.log("At POST to /register here is the req.body ..... : ", req.body);
-  const { username, password } = req.body;
-  if (!username || !password) {
-    return res
-      .status(400)
-      .json({ error: "Both username and password are required" });
-  }
-  pool.query(
-    "SELECT * FROM users WHERE username = $1",
-    [username],
-    (err, result) => {
-      if (err) {
-        return done(err);
-      }
+// app.post("/register", (req, res) => {
+//   console.log("At POST to /register here is the req.body ..... : ", req.body);
+//   const { username, password } = req.body;
+//   if (!username || !password) {
+//     return res
+//       .status(400)
+//       .json({ error: "Both username and password are required" });
+//   }
+//   pool.query(
+//     "SELECT * FROM users WHERE username = $1",
+//     [username],
+//     (err, result) => {
+//       if (err) {
+//         return done(err);
+//       }
 
-      const user = result.rows[0];
-      if (user) {
-        return res.json({ error: "Please select another username" });
-      }
-    }
-  );
+//       const user = result.rows[0];
+//       if (user) {
+//         return res.json({ error: "Please select another username" });
+//       }
+//     }
+//   );
 
-  const hashed_password = bcrypt.hashSync(password, 12);
-  pool.query(
-    "INSERT INTO users (username, password_hash) VALUES ($1, $2)",
-    [username, hashed_password],
-    (err, result) => {
-      if (err) {
-        console.error("Error inserting user into the database", err);
-        return res.status(500).json({ error: "Internal server error" });
-      } else {
-        return res.status(201).json({ message: "User created successfully" });
-      }
-    }
-  );
-});
+//   const hashed_password = bcrypt.hashSync(password, 12);
+//   pool.query(
+//     "INSERT INTO users (username, password_hash) VALUES ($1, $2)",
+//     [username, hashed_password],
+//     (err, result) => {
+//       if (err) {
+//         console.error("Error inserting user into the database", err);
+//         return res.status(500).json({ error: "Internal server error" });
+//       } else {
+//         return res.status(201).json({ message: "User created successfully" });
+//       }
+//     }
+//   );
+// });
 
 app.post(
   "/login",
@@ -207,142 +218,135 @@ app.post(
   }
 );
 
-app.get("/logout", (req, res) => {
-  if (req.isAuthenticated()) {
-    console.log("At GET /logout... Yes, indeed!");
-  } else {
-    console.log("At GET /logout... No, not at all!");
-  }
+// app.get("/logout", (req, res) => {
+//   if (req.isAuthenticated()) {
+//     console.log("At GET /logout... Yes, indeed!");
+//   } else {
+//     console.log("At GET /logout... No, not at all!");
+//   }
 
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    } else {
-      res.json({ logoutSuccessful: true });
-    }
-  });
-});
+//   req.logout((err) => {
+//     if (err) {
+//       return next(err);
+//     } else {
+//       res.json({ logoutSuccessful: true });
+//     }
+//   });
+// });
 
-app.get("/users", (req, res) => {
-  if (req.isAuthenticated()) {
-    console.log("At GET /users... Yes, indeed!");
-  } else {
-    console.log("At GET /users... No, not at all!");
-  }
+// app.get("/users", (req, res) => {
+//   if (req.isAuthenticated()) {
+//     console.log("At GET /users... Yes, indeed!");
+//   } else {
+//     console.log("At GET /users... No, not at all!");
+//   }
 
-  // Use COUNT() to get the total number of users
-  pool.query(
-    "SELECT COUNT(*) as total_users FROM users; SELECT * FROM users;",
-    (err, result) => {
-      if (err) {
-        console.error("Error executing SQL query", err);
-        res.status(500).json({ error: "Internal server error" });
-      } else {
-        // Extract the count from the first query result
-        const totalUsers = result[0].rows[0].total_users;
+//   // Use COUNT() to get the total number of users
+//   pool.query(
+//     "SELECT COUNT(*) as total_users FROM users; SELECT * FROM users;",
+//     (err, result) => {
+//       if (err) {
+//         console.error("Error executing SQL query", err);
+//         res.status(500).json({ error: "Internal server error" });
+//       } else {
+//         // Extract the count from the first query result
+//         const totalUsers = result[0].rows[0].total_users;
 
-        // Extract user data from the second query result
-        const users = result[1].rows;
+//         // Extract user data from the second query result
+//         const users = result[1].rows;
 
-        // Create a response object with both the count and user data
-        const response = {
-          total_users: totalUsers,
-          users: users,
-        };
+//         // Create a response object with both the count and user data
+//         const response = {
+//           total_users: totalUsers,
+//           users: users,
+//         };
 
-        return res.json(response);
-      }
-    }
-  );
-});
+//         return res.json(response);
+//       }
+//     }
+//   );
+// });
 
-app.put("/users/:id", (req, res) => {
-  const userId = req.params.id;
-  const { username, password } = req.body;
+// app.put("/users/:id", (req, res) => {
+//   const userId = req.params.id;
+//   const { username, password } = req.body;
 
-  if (!username || !password) {
-    return res
-      .status(400)
-      .json({ error: "Both username and password are required" });
-  }
+//   if (!username || !password) {
+//     return res
+//       .status(400)
+//       .json({ error: "Both username and password are required" });
+//   }
 
-  pool.query(
-    "UPDATE users SET username = $1, password = $2 WHERE id = $3",
-    [username, password, userId],
-    (err, result) => {
-      if (err) {
-        console.error("Error updating user in the database", err);
-        res.status(500).json({ error: "Internal server error" });
-      } else {
-        res.json({ message: "User updated successfully" });
-      }
-    }
-  );
-});
+//   pool.query(
+//     "UPDATE users SET username = $1, password = $2 WHERE id = $3",
+//     [username, password, userId],
+//     (err, result) => {
+//       if (err) {
+//         console.error("Error updating user in the database", err);
+//         res.status(500).json({ error: "Internal server error" });
+//       } else {
+//         res.json({ message: "User updated successfully" });
+//       }
+//     }
+//   );
+// });
 
-app.delete("/users/:id", (req, res) => {
-  const userId = req.params.id;
+// app.delete("/users/:id", (req, res) => {
+//   const userId = req.params.id;
 
-  pool.query("DELETE FROM users WHERE id = $1", [userId], (err, result) => {
-    if (err) {
-      console.error("Error deleting user from the database", err);
-      res.status(500).json({ error: "Internal server error" });
-    } else {
-      res.json({ message: "User deleted successfully" });
-    }
-  });
-});
+//   pool.query("DELETE FROM users WHERE id = $1", [userId], (err, result) => {
+//     if (err) {
+//       console.error("Error deleting user from the database", err);
+//       res.status(500).json({ error: "Internal server error" });
+//     } else {
+//       res.json({ message: "User deleted successfully" });
+//     }
+//   });
+// });
 
-//
-//
+// app.post("/yelp-data/:location", async (req, res) => {
+//   let locationSearchTerm = req.params.location;
+//   console.log(
+//     "At POST to /yelp-data/:location here is the req.body ..... : ",
+//     req.body
+//   );
+//   const { searchOffset, searchIsOpenNow, searchSortBy, searchPrice } = req.body;
+//   let updatedSearchPrice;
+//   switch (searchPrice) {
+//     case 1:
+//       updatedSearchPrice = "&price=1";
+//       break;
+//     case 2:
+//       updatedSearchPrice = "&price=1&price=2";
+//       break;
+//     case 3:
+//       updatedSearchPrice = "&price=1&price=2&price=3";
+//       break;
+//     default:
+//       updatedSearchPrice = "&price=1&price=2&price=3&price=4";
+//   }
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the nightlife server!");
-});
-
-app.post("/yelp-data/:location", async (req, res) => {
-  let locationSearchTerm = req.params.location;
-  console.log(
-    "At POST to /yelp-data/:location here is the req.body ..... : ",
-    req.body
-  );
-  const { searchOffset, searchIsOpenNow, searchSortBy, searchPrice } = req.body;
-  let updatedSearchPrice;
-  switch (searchPrice) {
-    case 1:
-      updatedSearchPrice = "&price=1";
-      break;
-    case 2:
-      updatedSearchPrice = "&price=1&price=2";
-      break;
-    case 3:
-      updatedSearchPrice = "&price=1&price=2&price=3";
-      break;
-    default:
-      updatedSearchPrice = "&price=1&price=2&price=3&price=4";
-  }
-
-  const url = `https://api.yelp.com/v3/businesses/search?location=${locationSearchTerm}${updatedSearchPrice}&open_now=${searchIsOpenNow}&sort_by=${searchSortBy}&limit=5&offset=${searchOffset}`;
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${API_KEY}`,
-    },
-  };
-  try {
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-  return;
-});
+//   const url = `https://api.yelp.com/v3/businesses/search?location=${locationSearchTerm}${updatedSearchPrice}&open_now=${searchIsOpenNow}&sort_by=${searchSortBy}&limit=5&offset=${searchOffset}`;
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       accept: "application/json",
+//       Authorization: `Bearer ${API_KEY}`,
+//     },
+//   };
+//   try {
+//     const response = await fetch(url, options);
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     const data = await response.json();
+//     res.json(data);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+//   return;
+// });
 
 // --------------------------------------------- //
 // ------------------  SERVER  ----------------- //
